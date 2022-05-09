c610516b46cbe9fc9dbe6fae1c2c2c41b9ea64fd
1327-Imai <k021g1327@g.neec.ac.jp>
2022-05-09 09:18:50 +0900

    確認課題3-7まで

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,12 +1,15 @@
 ﻿#include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
-
-using namespace DirectX;
+#include "PrimitiveDrawer.h"
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+
+	delete debugCamera_;
+
+}
 
 void GameScene::Initialize() {
 
@@ -14,9 +17,202 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	debugCamera_ = new DebugCamera(1280 , 720);
+
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
+
+#pragma region//アフィン変換
+#pragma region//移動
+	//原点からの距離を取得
+	distanceFromOrigin = vertex[0];
+
+	//原点まで移動する(現在の座標から原点からの距離を引く)
+	for (int i = 0; i < 8; i++) {
+		vertex[i] -= distanceFromOrigin;
+	}
+
+	//アフィン変換をする
+	for (int i = 0; i < 8; i++) {
+		boxMoved[i].x =
+			affineTransrate.m[0][0] * vertex[i].x +
+			affineTransrate.m[0][1] * vertex[i].y +
+			affineTransrate.m[0][2] * vertex[i].z +
+			affineTransrate.m[0][3] * 1;
+
+		boxMoved[i].y =
+			affineTransrate.m[1][0] * vertex[i].x +
+			affineTransrate.m[1][1] * vertex[i].y +
+			affineTransrate.m[1][2] * vertex[i].z +
+			affineTransrate.m[1][3] * 1;
+
+		boxMoved[i].z =
+			affineTransrate.m[2][0] * vertex[i].x +
+			affineTransrate.m[2][1] * vertex[i].y +
+			affineTransrate.m[2][2] * vertex[i].z +
+			affineTransrate.m[2][3] * 1;
+	};
+
+	//原点からの距離を足して元の位置に戻す
+	for (int i = 0; i < 8; i++) {
+		vertex[i] += distanceFromOrigin;
+		boxMoved[i] += distanceFromOrigin;
+	}
+#pragma endregion//移動
+
+#pragma region//X軸回転
+	//原点からの距離を取得
+	distanceFromOrigin = vertex[0];
+
+	//原点まで移動する(現在の座標から原点からの距離を引く)
+	for (int i = 0; i < 8; i++) {
+		vertex[i] -= distanceFromOrigin;
+	}
+
+	//アフィン変換をする
+	for (int i = 0; i < 8; i++) {
+		boxRotatedX[i].x =
+			affineRotationX.m[0][0] * vertex[i].x +
+			affineRotationX.m[0][1] * vertex[i].y +
+			affineRotationX.m[0][2] * vertex[i].z +
+			affineRotationX.m[0][3] * 1;
+
+		boxRotatedX[i].y =
+			affineRotationX.m[1][0] * vertex[i].x +
+			affineRotationX.m[1][1] * vertex[i].y +
+			affineRotationX.m[1][2] * vertex[i].z +
+			affineRotationX.m[1][3] * 1;
+
+		boxRotatedX[i].z =
+			affineRotationX.m[2][0] * vertex[i].x +
+			affineRotationX.m[2][1] * vertex[i].y +
+			affineRotationX.m[2][2] * vertex[i].z +
+			affineRotationX.m[2][3] * 1;
+	};
+
+	//原点からの距離を足して元の位置に戻す
+	for (int i = 0; i < 8; i++) {
+		vertex[i] += distanceFromOrigin;
+		boxRotatedX[i] += distanceFromOrigin;
+	}
+#pragma endregion//X軸回転
+
+#pragma region//Y軸回転
+	//原点からの距離を取得
+	distanceFromOrigin = vertex[0];
+
+	//原点まで移動する(現在の座標から原点からの距離を引く)
+	for (int i = 0; i < 8; i++) {
+		vertex[i] -= distanceFromOrigin;
+	}
+
+	//アフィン変換をする
+	for (int i = 0; i < 8; i++) {
+		boxRotatedY[i].x =
+			affineRotationY.m[0][0] * vertex[i].x +
+			affineRotationY.m[0][1] * vertex[i].y +
+			affineRotationY.m[0][2] * vertex[i].z +
+			affineRotationY.m[0][3] * 1;
+
+		boxRotatedY[i].y =
+			affineRotationY.m[1][0] * vertex[i].x +
+			affineRotationY.m[1][1] * vertex[i].y +
+			affineRotationY.m[1][2] * vertex[i].z +
+			affineRotationY.m[1][3] * 1;
+
+		boxRotatedY[i].z =
+			affineRotationY.m[2][0] * vertex[i].x +
+			affineRotationY.m[2][1] * vertex[i].y +
+			affineRotationY.m[2][2] * vertex[i].z +
+			affineRotationY.m[2][3] * 1;
+	};
+
+	//原点からの距離を足して元の位置に戻す
+	for (int i = 0; i < 8; i++) {
+		vertex[i] += distanceFromOrigin;
+		boxRotatedY[i] += distanceFromOrigin;
+	}
+#pragma endregion//Y軸回転
+
+#pragma region//Z軸回転
+	//原点からの距離を取得
+	distanceFromOrigin = vertex[0];
+
+	//原点まで移動する(現在の座標から原点からの距離を引く)
+	for (int i = 0; i < 8; i++) {
+		//vertex[i] -= distanceFromOrigin;
+	}
+
+	//アフィン変換をする
+	for (int i = 0; i < 8; i++) {
+		boxRotatedZ[i].x =
+			affineRotationZ.m[0][0] * vertex[i].x +
+			affineRotationZ.m[0][1] * vertex[i].y +
+			affineRotationZ.m[0][2] * vertex[i].z +
+			affineRotationZ.m[0][3] * 1;
+
+		boxRotatedZ[i].y =
+			affineRotationZ.m[1][0] * vertex[i].x +
+			affineRotationZ.m[1][1] * vertex[i].y +
+			affineRotationZ.m[1][2] * vertex[i].z +
+			affineRotationZ.m[1][3] * 1;
+
+		boxRotatedZ[i].z =
+			affineRotationZ.m[2][0] * vertex[i].x +
+			affineRotationZ.m[2][1] * vertex[i].y +
+			affineRotationZ.m[2][2] * vertex[i].z +
+			affineRotationZ.m[2][3] * 1;
+	};
+
+	//原点からの距離を足して元の位置に戻す
+	for (int i = 0; i < 8; i++) {
+		vertex[i] += distanceFromOrigin;
+		boxRotatedZ[i] += distanceFromOrigin;
+	}
+#pragma endregion//Z軸回転
+
+#pragma region//拡大縮小
+	//原点からの距離を取得
+	distanceFromOrigin = vertex[0];
+
+	//原点まで移動する(現在の座標から原点からの距離を引く)
+	for (int i = 0; i < 8; i++) {
+		vertex[i] -= distanceFromOrigin;
+	}
+
+	//アフィン変換をする
+	for (int i = 0; i < 8; i++) {
+		boxEnlarged[i].x = affineScale.m[0][0] * vertex[i].x +
+			affineScale.m[0][1] * vertex[i].y +
+			affineScale.m[0][2] * vertex[i].z +
+			affineScale.m[0][3] * 1;
+
+		boxEnlarged[i].y = affineScale.m[1][0] * vertex[i].x +
+			affineScale.m[1][1] * vertex[i].y +
+			affineScale.m[1][2] * vertex[i].z +
+			affineScale.m[1][3] * 1;
+
+		boxEnlarged[i].z = affineScale.m[2][0] * vertex[i].x +
+			affineScale.m[2][1] * vertex[i].y +
+			affineScale.m[2][2] * vertex[i].z +
+			affineScale.m[2][3] * 1;
+	};
+
+	//原点からの距離を足して元の位置に戻す
+	for (int i = 0; i < 8; i++) {
+		vertex[i] += distanceFromOrigin;
+		boxEnlarged[i] += distanceFromOrigin;
+	}
+#pragma endregion//拡大縮小
+
+#pragma endregion//アフィン変換
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+
+	debugCamera_->Update();
+
+}
 
 void GameScene::Draw() {
 
@@ -44,6 +240,15 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+
+	for (int i = 0; i < 12; i++) {
+		PrimitiveDrawer::GetInstance()->DrawLine3d(vertex[edgeList[i][0]] , vertex[edgeList[i][1]] , WHITE);
+		PrimitiveDrawer::GetInstance()->DrawLine3d(boxMoved[edgeList[i][0]] , boxMoved[edgeList[i][1]] , BLUE);
+		PrimitiveDrawer::GetInstance()->DrawLine3d(boxRotatedX[edgeList[i][0]] , boxRotatedX[edgeList[i][1]] , RED);
+		PrimitiveDrawer::GetInstance()->DrawLine3d(boxRotatedY[edgeList[i][0]] , boxRotatedY[edgeList[i][1]] , YELLOW);
+		PrimitiveDrawer::GetInstance()->DrawLine3d(boxRotatedZ[edgeList[i][0]] , boxRotatedZ[edgeList[i][1]] , PURPLE);
+		PrimitiveDrawer::GetInstance()->DrawLine3d(boxEnlarged[edgeList[i][0]] , boxEnlarged[edgeList[i][1]] , GREEN);
+	}
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -9,17 +9,17 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-#include <DirectXMath.h>
+#include "DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
 /// </summary>
 class GameScene {
 
-  public: // メンバ関数
-	/// <summary>
-	/// コンストクラタ
-	/// </summary>
+public: // メンバ関数
+  /// <summary>
+  /// コンストクラタ
+  /// </summary>
 	GameScene();
 
 	/// <summary>
@@ -42,11 +42,101 @@ class GameScene {
 	/// </summary>
 	void Draw();
 
-  private: // メンバ変数
+private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
+
+	DebugCamera* debugCamera_ = nullptr;
+
+	const double PI = 3.141592;
+
+	Vector3 vertex[8] = {
+		{10.0f , 10.0f , 10.0f} ,
+		{10.0f , 20.0f , 10.0f} ,
+		{20.0f , 20.0f , 10.0f} ,
+		{20.0f , 10.0f , 10.0f} ,
+		{10.0f , 10.0f , 20.0f} ,
+		{10.0f , 20.0f , 20.0f} ,
+		{20.0f , 20.0f , 20.0f} ,
+		{20.0f , 10.0f , 20.0f} ,
+	};
+
+	Vector3 boxMoved[8];
+	Vector3 boxRotatedX[8];
+	Vector3 boxRotatedY[8];
+	Vector3 boxRotatedZ[8];
+	Vector3 boxEnlarged[8];
+
+	int edgeList[12][2] = {
+		{0 , 1} ,
+		{1 , 2} ,
+		{2 , 3} ,
+		{3 , 0} ,
+		{0 , 4} ,
+		{1 , 5} ,
+		{2 , 6} ,
+		{3 , 7} ,
+		{4 , 5} ,
+		{5 , 6} ,
+		{6 , 7} ,
+		{7 , 4} ,
+	};
+
+	float transrateX = 10.0f;
+	float transrateY = 10.0f;
+	float transrateZ = 10.0f;
+	float scaleX = 2.0f;
+	float scaleY = 2.0f;
+	float scaleZ = 2.0f;
+	float angleX = -PI / 4;
+	float angleY = PI / 4;
+	float angleZ = PI / 6;
+
+
+	Matrix4 affineTransrate = {
+		1.0f , 0.0f , 0.0f , transrateX ,
+		0.0f , 1.0f , 0.0f , transrateY ,
+		0.0f , 0.0f , 1.0f , transrateZ ,
+		0.0f , 0.0f , 0.0f , 1.0f};
+
+	Matrix4 affineRotationX = {
+		1.0f , 0.0f , 0.0f , 0.0f ,
+		0.0f , cos(angleX) , -sin(angleX) , 0.0f ,
+		0.0f , sin(angleX) , cos(angleX) , 0.0f ,
+		0.0f , 0.0f , 0.0f , 1.0f
+	};
+
+	Matrix4 affineRotationY = {
+		cos(angleY) , 0.0f , sin(angleY) , 0.0f ,
+		0.0f , 1.0f , 0.0f , 0.0f ,
+		-sin(angleY) , 0.0f , cos(angleY) , 0.0f ,
+		0.0f , 0.0f , 0.0f , 1.0f
+	};
+
+	Matrix4 affineRotationZ = {
+		cos(angleZ) , -sin(angleZ) , 0.0f , 0.0f ,
+		sin(angleZ) , cos(angleZ) , 0.0f , 0.0f ,
+		0.0f , 0.0f , 1.0f , 0.0f ,
+		0.0f , 0.0f , 0.0f , 1.0f
+	};
+
+	Matrix4 affineScale = {
+		scaleX , 0.0f , 0.0f , 0.0f ,
+		0.0f , scaleY , 0.0f , 0.0f ,
+		0.0f , 0.0f , scaleZ , 0.0f ,
+		0.0f , 0.0f , 0.0f , 1.0f
+	};
+
+	Vector3 distanceFromOrigin = {0 , 0 , 0};
+
+	const Vector4 WHITE = {255 , 255 , 255 , 255};
+	const Vector4 RED = {255 , 0 , 0 , 255};
+	const Vector4 YELLOW = {255 , 255 , 0 , 255};
+	const Vector4 PURPLE = {255 , 0 , 255 , 255};
+	const Vector4 GREEN = {0 , 255 , 0 , 255};
+	const Vector4 BLUE = {0 , 0 , 255 , 255};
 
 	/// <summary>
 	/// ゲームシーン用
